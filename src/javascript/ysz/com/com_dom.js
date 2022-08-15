e89c1d3cc510ef7e684d7db3f0602dbbc0eee93b
Admin <yszoo@hotmail.com>
2022-08-15 15:50:50 +0800

    1

--- src/javascript/ysz/com/com_dom.js
+++ src/javascript/ysz/com/com_dom.js
@@ -1,14 +1,14 @@
 ;
 ! function (win) {
-    win.YSZ.prototype.com_dom = function (tag, attr, binddata) {
+    win.YSZ.prototype.com_dom = function (tagname, attr, binddata) {
         var y = this;
         layui.use(['jquery', 'layer', 'yszutil'], function () {
             var $ = layui.jquery,
                 yu = layui.yszutil;
             if (this.elem) {
-                var $tag = $(tag).appendTo($(this.elem)).attr({ id: this.id }).attr(attr).data('binddata', binddata);
+                var $tag = $(tagname).appendTo($(this.elem)).attr({ id: this.id }).attr(attr).data('binddata', binddata);
                 $tag.click(function () {
-
+                    yu.events.call(this, y, 'click');
                 });
             }
         });
