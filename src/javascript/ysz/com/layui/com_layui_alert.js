cc660c7b9c6ec28aa10be10f75cff5092238dd33
Admin <yszoo@hotmail.com>
2023-01-06 17:09:25 +0800

    1

---
+++ src/javascript/ysz/com/layui/com_layui_alert.js
@@ -0,0 +1,19 @@
+;
+! function (win) {
+    win.YSZ.prototype.com_layui_alert = function (msg, options, func, showOnTop, callback) {
+        var y = this;
+        layui.use(['jquery', 'layer', 'yszutil'], function () {
+            var $ = layui.jquery,
+                yu = layui.yszutil,
+                alertindex = layer.alert(msg, options, func);
+            if (showOnTop || showOnTop === undefined) {
+                $('.layui-layer-shade').css('z-index', yu.top_zindex);
+                $('.layui-layer-shade').next('[type="dialog"]').css('z-index', yu.top_zindex);
+            }
+            if (typeof callback === 'function') {
+                callback.call(ythis, alertindex);
+            }
+        });
+        return this;
+    };
+}(window); 
